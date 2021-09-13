2b01980422941fdfc34b1e20d68e3d18663f096c
jenpyle <jennypyle440@gmail.com>
2021-09-13 16:09:42 -0600

    Add JSDoc

--- models.js
+++ models.js
@@ -1,48 +1,55 @@
+/**
+ * @file Mongoose Models made for Users and Movies.
+ * The "business logic layer", translates between the database and the API
+ * The models are imported into index.js so the API endpoints can make
+ * use of them to query the DB according to the schemas
+ */
 //Schemas for users and movies
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
-  Title: { type: String, required: true },
-  Description: { type: String, required: true },
-  Genre: {
-    //Genre becomes "genres"
-    Name: String,
-    Description: String,
-  },
-  Director: {
-    Name: String,
-    Bio: String,
-  },
-  Actors: [String],
-  ImagePath: String,
-  Featured: Boolean,
+	Title: { type: String, required: true },
+	Description: { type: String, required: true },
+	Genre: {
+		Name: String,
+		Description: String,
+	},
+	Director: {
+		Name: String,
+		Bio: String,
+	},
+	Actors: [String],
+	ImagePath: String,
+	Featured: Boolean,
 });
 
 let userSchema = mongoose.Schema({
-  Username: { type: String, required: true },
-  Password: { type: String, required: true },
-  Email: { type: String, required: true },
-  Birthday: Date,
-  FavoriteMovies: [
-    { type: mongoose.Schema.Types.ObjectId, ref: 'Movie' },
-  ] /*this array of IDs refrence the "db.movies" collection....links "Users" collection to "Movies" collection
-  ------We use the singular “Movie” because that is the name of the model which links the movieSchema to its database collection*/,
-  ToWatch: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+	Username: { type: String, required: true },
+	Password: { type: String, required: true },
+	Email: { type: String, required: true },
+	Birthday: Date,
+	/**
+	 * This array of IDs refrence the "db.movies" collection....links "Users" collection to "Movies" collection
+	 * Singular “Movie” bc that is the name of the model which links the movieSchema to its database collection
+	 */
+	FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+	ToWatch: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
 });
 
 userSchema.statics.hashPassword = (password) => {
-  return bcrypt.hashSync(password, 10);
+	return bcrypt.hashSync(password, 10);
 };
 
 userSchema.methods.validatePassword = function (password) {
-  return bcrypt.compareSync(password, this.Password);
+	return bcrypt.compareSync(password, this.Password);
 };
 
-//This will create collections called “db.movies” and “db.users” within the MongoDB database you created in the previous Exercise
-let Movie = mongoose.model('Movie', movieSchema); //Moive becomes "movies". Any titles you pass through will come out on the other side as lowercase and pluralized
+//Create collections “db.movies” and “db.users” within the MongoDB database
+//Moive becomes "movies". Any titles you pass through will come out on the other side as lowercase and pluralized
+let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
 
-//This will let you then import these models into your “index.js” file
+//Export these models to use in “index.js”
 module.exports.Movie = Movie;
 module.exports.User = User;
--- passport.js
+++ passport.js
@@ -1,58 +1,61 @@
+/**
+ * @file Impliments Passport middleware tool which handles Authentication and Authorization for database access
+ */
 const passport = require('passport'),
-  LocalStrategy = require('passport-local')
-    .Strategy /*checks username and pass from HTTP request body and uses mongoose to check database */,
-  Models = require('./models.js'),
-  passportJWT = require('passport-jwt');
+	//checks username and pass from HTTP request body and uses mongoose to check database
+	LocalStrategy = require('passport-local').Strategy,
+	Models = require('./models.js'),
+	passportJWT = require('passport-jwt');
 
 let Users = Models.User,
-  JWTStrategy = passportJWT.Strategy,
-  ExtractJWT = passportJWT.ExtractJwt;
+	JWTStrategy = passportJWT.Strategy,
+	ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(
-  new LocalStrategy(
-    {
-      usernameField: 'Username',
-      passwordField: 'Password',
-    },
-    (username, password, callback) => {
-      console.log(username + '  ' + password);
-      Users.findOne({ Username: username }, (error, user) => {
-        if (error) {
-          console.log(error);
-          return callback(error);
-        }
+	new LocalStrategy(
+		{
+			usernameField: 'Username',
+			passwordField: 'Password',
+		},
+		(username, password, callback) => {
+			console.log(username + '  ' + password);
+			Users.findOne({ Username: username }, (error, user) => {
+				if (error) {
+					console.log(error);
+					return callback(error);
+				}
 
-        if (!user) {
-          console.log('incorrect username');
-          return callback(null, false, { message: 'Incorrect username or password.' });
-        }
-        //Hash any password entered by the user when logging in before comparing it to the password stored in MongoDB
-        if (!user.validatePassword(password)) {
-          console.log('incorrect password');
-          return callback(null, false, { message: 'Incorrect password.' });
-        }
+				if (!user) {
+					console.log('incorrect username');
+					return callback(null, false, { message: 'Incorrect username or password.' });
+				}
+				//Hash any password entered by the user when logging in before comparing it to the password stored in MongoDB
+				if (!user.validatePassword(password)) {
+					console.log('incorrect password');
+					return callback(null, false, { message: 'Incorrect password.' });
+				}
 
-        console.log('finished');
-        return callback(null, user);
-      });
-    }
-  )
+				console.log('finished');
+				return callback(null, user);
+			});
+		}
+	)
 );
 
 passport.use(
-  new JWTStrategy(
-    {
-      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'your_jwt_secret',
-    },
-    (jwtPayload, callback) => {
-      return Users.findById(jwtPayload._id)
-        .then((user) => {
-          return callback(null, user);
-        })
-        .catch((error) => {
-          return callback(error);
-        });
-    }
-  )
+	new JWTStrategy(
+		{
+			jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+			secretOrKey: 'your_jwt_secret',
+		},
+		(jwtPayload, callback) => {
+			return Users.findById(jwtPayload._id)
+				.then((user) => {
+					return callback(null, user);
+				})
+				.catch((error) => {
+					return callback(error);
+				});
+		}
+	)
 );
